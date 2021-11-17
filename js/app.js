fa942c741cf4815448bf149ddd608bc208a272be
NicholasKuzya <kuzievkolaf@gmail.com>
2021-11-17 10:00:50 +0500

    First commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+$(function($){
+    $('.b-counter-info-numbers').each(function(){
+        $(this).prop('Counter', 0).animate({
+            Counter:$(this).text()
+        },
+        {
+           duration: 5000,
+           easing: "swing",
+           step:function(now){
+               $(this).text(Math.ceil(now));
+           } 
+        });
+    });
+
+    $(window).on('load', function () {
+        $(".preloader-item").fadeOut();
+        $("#preload").delay(200).fadeOut("slow");
+    
+    $('.b-gallery-controls li').on('click', function () {
+        $('.b-gallery-controls li').removeClass('active');
+        $(this).addClass('active');
+    });
+    if ($('.b-gallery-filter').length > 0) {
+        var containerEl = document.querySelector('.b-gallery-filter');
+        var mixer = mixitup(containerEl);
+    }
+    $(".testimonial-slider").owlCarousel({
+    items: 2,
+    dots: true,
+    autoplay: true,
+    loop: true,
+    smartSpeed: 1200,
+    margin: 0,
+    responsive: {
+        320: {
+            items: 1,
+        },
+        480: {
+            items: 1,
+        },
+        992: {
+            items: 2,
+        }
+    }
+});
+});
+
+});
+
+
+
